2bde5d05d8e90e3813908f9271b084c9795437c7
Shane Kilkelly <shane@kilkelly.me>
2017-01-02 16:20:18 +0000

    Scroll stuff

--- app/src/components/Page.vue
+++ app/src/components/Page.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="page" class="">
+  <div id="page" class="container">
     <div class="columns is-mobile">
-      <div class="column is-8 is-offset-2 content">
+      <div id="page-wrapper" class="column is-8 is-offset-2 content">
 
         <iframe frameborder="0"
                 name="renderedPage"
@@ -27,10 +27,13 @@
 </script>
 
 <style scoped>
+ page-wrapper {
+   height: 100%;
+   overflow: scroll;
+ }
  iframe {
    width: 100%;
    height: 100%;
    min-height: 600px;
-   overflow: scroll;
  }
 </style>
